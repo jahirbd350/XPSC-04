1fb8fe8f10c7ab12d4f39f8c608247b9547d047f
Md Jahirul Islam <jahirbd350@gmail.com>
2024-03-22 22:36:36 +0600

    Week 02, Day-01

---
+++ Weel-02/Day-01/A_Plus_or_Minus.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long;
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b == c)
+            cout<<"+"<<'\n';
+        else
+            cout<<"-"<<'\n';
+    }
+    
+    return 0;
+}
